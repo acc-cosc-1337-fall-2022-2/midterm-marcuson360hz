ee5f61d762e143da7baf747d26eb3004df13e28e
marcuson360hz <marcus.darensbourg@g.austincc.edu>
2022-10-16 00:13:51 -0500

    Midterm Question 4

--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -1,5 +1,42 @@
-//
+#include "question4.h"
+#include <iostream>
+
+
 bool test_config()
 {
     return true;
+}
+
+bool is_prime(int prime_param){
+
+    if (prime_param <= 1){
+
+        return false;
+    }
+        
+    for (int i = 2; i < prime_param; i++){
+
+        if (prime_param % i == 0){
+
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+std::vector<int> get_primes(int num){
+
+    std::vector<int> numVector;
+
+    for (int i = 2; i <= num; i++){
+
+        if (is_prime(i)){
+
+            numVector.push_back(i) + " ";
+        }
+    }
+
+    return numVector;
 }
