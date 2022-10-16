adf77352bd71fcdd54b52d4eb3e452d4c841ca4f
marcuson360hz <marcus.darensbourg@g.austincc.edu>
2022-10-15 21:33:15 -0500

    Midterm Question 3

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -1,6 +1,23 @@
 #include "question3.h"
+#include <string>
 
 bool test_config()
 {
     return true;
+}
+
+bool is_palindrome(const std::string &phrase){
+
+    std::string reverse_phrase = phrase;
+    reverse(reverse_phrase.begin(), reverse_phrase.end());
+
+    if (reverse_phrase == phrase){
+
+        return true;
+    }
+
+    else{
+
+        return false;
+    }
 }
